271a4405cade03945c79c3a493b156820caabb7e
YanLozovskyi <yanlozovskiy@gmail.com>
2023-12-03 01:05:51 +0200

    changed routing to users

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", require("./routes/api/usersRoutes"));
+app.use("/users", require("./routes/api/usersRoutes"));
 app.use("/api/contacts", require("./routes/api/contactsRoutes"));
 
 app.use((req, res) => {
